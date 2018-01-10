c39fe2082787a128a059422cc83304883f9cab62
dylan <gbujieshi@gmail.com>
2018-01-10 14:33:13 +0800

    add index list
    as title

---
+++ packages/wx-indexlist/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="wx-indexlist">
+        <ul>
+            
+        </ul>
+        <ul>
+            
+        </ul>
+    </div>
+</template>
+
+<script>
+    const modal = weex.requireModule('modal');
+    const animation = weex.requireModule('animation');
+
+    export default {
+        props: {
+            
+        },
+
+        data () {
+            return {
+                
+            }
+        },
+
+        computed: {
+            
+        },
+
+        created () {
+            
+        },
+
+        methods: {
+            
+        },
+
+        watch: {
+            
+        }
+    }
+</script>
+
+<style scoped>
+    .wx-indexlist{
+        outline: 1px dashed blue;
+    }
+</style>
