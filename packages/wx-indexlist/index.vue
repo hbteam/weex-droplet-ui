7d506967b793f0865f10660e0ec4147afb80fcf2
dylan <gbujieshi@gmail.com>
2018-01-24 14:39:54 +0800

    commit

--- packages/wx-indexlist/index.vue
+++ packages/wx-indexlist/index.vue
@@ -1,21 +1,51 @@
 <template>
-    <div class="wx-indexlist">
-        <ul>
-            
-        </ul>
-        <ul>
-            
-        </ul>
+    <div class="wx-indexlist" >
+        <div class='mainList'>
+            <scroller class='scrollerContainer'>
+            <div class='eachCategory' v-for='category in indexList'>
+                <div><text>{{category.text}}</text></div>
+                <text v-for='item in category.list'>
+                    {{item.text}}
+                </text>
+            </div>
+            </scroller>
+        </div>
+        <div class='indexList'>
+            <div class='eachIndex' v-for='category in indexList'>
+                <text>{{category.text}}</text>
+            </div>
+        </div>
     </div>
 </template>
-
 <script>
     const modal = weex.requireModule('modal');
     const animation = weex.requireModule('animation');
 
     export default {
         props: {
-            
+            indexList: {
+              type: Array,
+              default: function () {
+                let list=[];
+                for(let i=0; i<26; i++){
+                    list.push({
+                        text:String.fromCharCode(65+i),
+                        list:[
+                            {
+                                text:String.fromCharCode(65+i)+1
+                            },{
+                                text:String.fromCharCode(65+i)+2
+                            },{
+                                text:String.fromCharCode(65+i)+3
+                            },{
+                                text:String.fromCharCode(65+i)+4
+                            }
+                        ]
+                    });
+                }
+                return list; 
+              }
+            }
         },
 
         data () {
@@ -44,6 +74,18 @@
 
 <style scoped>
     .wx-indexlist{
+        width: 750px;
+        height: 600px;
         outline: 1px dashed blue;
+        position: relative;
     }
+    .indexList{
+        position: fixed;
+        right: 0;
+        top: 0;
+    }
+    .scrollerContainer{
+        
+    }
+    
 </style>
