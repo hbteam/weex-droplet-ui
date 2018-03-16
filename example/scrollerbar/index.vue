f877fb566820e73ef8ba36ebaeb3f64cb57202d0
yangquan <yangquan@houbank.cn>
2018-03-16 18:52:14 +0800

    scrollbar update

--- example/scrollerbar/index.vue
+++ example/scrollerbar/index.vue
@@ -1,117 +1,36 @@
 <template>
-    <div>
-        <scroller class="wx-list" show-scrollbar="false">
-            <div
-                :class="[selectIndex == index ? 'select-cell' : 'wx-cell']"
-                :ref="'item'+index"
-                v-for="(item, index) in items" @click="changeTab(index)">
-                <text class="wx-text">{{ item }}</text>
-            </div> 
-        </scroller>
+    <div class="wx-demo">
+        <wx-scrollerbar 
+            :items="items"
+            width="250px" 
+            height="700px"
+            @wxChange="handleChange"
+            itemHeight="100px">
+        </wx-scrollerbar>
     </div>
 </template>
 <script>
-    var dom = weex.requireModule('dom')
-    var animation = weex.requireModule('animation')
+    import { WxScrollerbar } from '../../index';
     export default {
+        components: {
+            WxScrollerbar,
+        },
         data () {
             return {
-                items: ['保单信息1', '保单信息2', '保单信息3', '保单信息4', '保单信息5', '保单信息6',
-                    '保单信息7', '保单信息8', '保单信息9', '保单信息10', '保单信息11', '保单信息12','保单信息13'],
-                selectIndex: 0,
-                count: 0,
-                data: {
-                    pwidth: 250,
-                    pheight: 700,
-                    cheight: 100,
-                },
-                hiddenCount: 0,
-                maxHidden: 0,
+                items: ['保单信息1', '保单信息2', '保单信息3', '保单信息4', '保单信息5', '保单信息6','保单信息7', '保单信息8', '保单信息9', '保单信息10', '保单信息11', '保单信息12','保单信息13'],
             }
         },
-
-        created () {
-            // this.deviceHeight = weex.config.env.deviceHeight
-            this.count = Math.floor(this.data.pheight / this.data.cheight);
-            // 最大隐藏个数（共37条，一页10条，能隐藏37-10条）
-            this.maxHidden = this.items.length - this.count;
-        },
-
         methods: {
-            changeTab (index) {
-                this.selectIndex = index;
-                if (!index) return;
-                const middle = Math.floor(this.count / 2);
-                if (index >= middle) {
-                    this.hiddenCount = index - middle;
-                    console.log(index - middle)
-                    this.hiddenCount = this.getCanMove();
-                    this.triggerAnimation(-this.hiddenCount * this.data.cheight);
-                } else {
-                    this.hiddenCount = 0;
-                    this.triggerAnimation(0);
-                }
-            },
-
             /**
-             * 获取能移动多少条，不能超过总条数
+             * 点击回调
+             * @param  {[type]} item 被选中的项
              */
-            getCanMove () {
-                return this.hiddenCount > this.maxHidden ? this.maxHidden : this.hiddenCount;
-            },
-
-            triggerAnimation(top){
-                const index = top / 100;
-                if (index > 0) {
-                    const el = this.$refs['item' + (13-index)][0]
-                    dom.scrollToElement(el, {})
-                } else {
-                    const el = this.$refs['item' + (0-index)][0]
-                    dom.scrollToElement(el, {});
-                }
-                
-                
-                // let el = this.$refs.list;
-                // animation.transition(el, {
-                //     styles: {
-                //         transform: `translateY(${top}px)`,
-                //         transformOrigin: 'center center'
-                //     },
-                //     duration: 300,
-                //     timingFunction: 'ease-out',
-                //     needLayout: false,
-                //     delay: 0 //ms
-                // });
+            handleChange (item) {
+                console.log(item)
             }
         }
     }
 </script>
 <style scoped>
-    .wx-list {
-        width: 250px;
-        background-color: #969696;
-        height: 700px;
-        overflow: hidden;
-    }
-
-    .wx-cell {
-        width: 250px;
-        height: 100px;
-    }
-
-    .wx-text {
-        color: #4d4d4d;
-        font-size: 32px;
-        width: 250px;
-        height: 160px;
-        text-align: center;
-        line-height: 100px;
-    }
-
-    .select-cell {
-        width: 250px;
-        height: 100px;
-        font-size: 32px;
-        background-color: #ffffff;
-    }
+    
 </style>
--- packages/wx-scrollerbar/index.vue
+++ packages/wx-scrollerbar/index.vue
@@ -1,16 +1,135 @@
 <template>
     <div>
-
+        <scroller class="wx-list" 
+        :style="{width: width, height: height}" show-scrollbar="false">
+            <div
+                :style="{width: width, height: itemHeight}"
+                :class="[selectIndex == index ? 'select-cell' : 'wx-cell']"
+                :ref="'item'+index"
+                v-for="(item, index) in items" @click="changeTab(index)">
+                <text class="wx-text">{{ item }}</text>
+            </div> 
+        </scroller>
     </div>
 </template>
 <script>
+    var dom = weex.requireModule('dom')
+    var animation = weex.requireModule('animation')
     export default {
+        props: {
+            items: {
+                type: Array,
+                default: function () {
+                    return []
+                },
+                required: true
+            },
+            wxChange: {
+                type: Function,
+                required: true
+            },
+            width: {
+                type: String,
+                default: '250px'
+            },
+            height: {
+                type: String,
+                default: '700px'
+            },
+            itemHeight: {
+                type: String,
+                default: '100px'
+            },
+        },
+
         data () {
             return {
-                msg: 'hello vue'
+                selectIndex: 0,
+                count: 0,
+                data: {
+                    pwidth: 0,
+                    pheight: 0,
+                    cheight: 0,
+                },
+                hiddenCount: 0,
+                maxHidden: 0,
+            }
+        },
+
+        created () {
+            this.getData();
+            // this.deviceHeight = weex.config.env.deviceHeight
+            this.count = Math.floor(this.data.pheight / this.data.cheight);
+            // 最大隐藏个数（共37条，一页10条，能隐藏37-10条）
+            this.maxHidden = this.items.length - this.count;
+        },
+
+        methods: {
+
+            getData () {
+                this.data = {
+                    pwidth: Number(this.width.replace('px', '')),
+                    pheight: Number(this.height.replace('px', '')),
+                    cheight: Number(this.itemHeight.replace('px', ''))
+                };
+            },
+
+            changeTab (index) {
+                this.selectIndex = index;
+                if (!index) return;
+                const middle = Math.floor(this.count / 2);
+                if (index >= middle) {
+                    this.hiddenCount = index - middle;
+                    this.hiddenCount = this.getCanMoves();
+                    this.triggerAnimation(-this.hiddenCount * this.data.cheight);
+                } else {
+                    this.hiddenCount = 0;
+                    this.triggerAnimation(0);
+                }
+                this.$emit('wxChange', this.items[index]);
+            },
+
+            /**
+             * 获取能移动多少条，不能超过总条数
+             */
+            getCanMoves () {
+                return this.hiddenCount > this.maxHidden ? this.maxHidden : this.hiddenCount;
+            },
+
+            triggerAnimation(top){
+                const index = top / 100;
+                if (index > 0) {
+                    const el = this.$refs['item' + (13-index)][0]
+                    dom.scrollToElement(el, {})
+                } else {
+                    const el = this.$refs['item' + (0-index)][0]
+                    dom.scrollToElement(el, {});
+                }
             }
         }
     }
 </script>
-<style>
+<style scoped>
+    .wx-list {
+        background-color: #969696;
+        overflow: hidden;
+    }
+
+    .wx-cell {
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .wx-text {
+        color: #4d4d4d;
+        font-size: 32px;
+    }
+
+    .select-cell {
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        background-color: #ffffff;
+    }
 </style>
