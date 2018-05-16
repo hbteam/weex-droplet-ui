a9a3ac663492fcc5c239ec0a115e110d5a746bff
yangquan <yangquan@houbank.cn>
2018-05-16 14:17:15 +0800

    update header demo

--- example/header/index.vue
+++ example/header/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="wx-demo">
-        <wx-header textFontSize="40px" textColor="#333" arrowColor="#333" arrowSize="30px" :hasBottom="true" text="帮助" @wxBack="wxBackHandle"></wx-header>
+        <wx-header textFontSize="40px" textColor="#333" arrowColor="#333" arrowSize="30px" :hasBottom="true" :useDefaultBack="false" text="帮助" @wxBack="wxBackHandle"></wx-header>
 
          <wx-header text="帮助" @wxBack="wxBackHandle"></wx-header>
     </div>
