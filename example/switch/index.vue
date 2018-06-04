e1770d2f9db68e0841371c8e9e6d724d33fc46d5
yangquan <yangquan@houbank.cn>
2018-06-04 11:12:50 +0800

    update demo

--- example/switch/index.vue
+++ example/switch/index.vue
@@ -1,32 +1,62 @@
 <template>
     <div class="wx-demo">
-        <wx-switch 
-            class="switch" 
-            v-model="value" 
-            @wxChange="handleChange" 
-            :disabled="false">
-        </wx-switch>
-        <text>{{ value }}</text>
+        <wx-cell 
+            text="有无住房"
+            width="690px"
+            height="120px"
+            textColor="#333"
+            textFontSize="32px"
+            :hasArrow="false"
+            @wxClick="handleClick">
+                <wx-switch 
+                    slot="right"
+                    class="switch" 
+                    v-model="value1" 
+                    @wxChange="handleChange" 
+                    :disabled="false">
+                </wx-switch>
+        </wx-cell>
+        <wx-cell 
+            text="有公积金"
+            width="690px"
+            height="120px"
+            textColor="#333"
+            textFontSize="32px"
+            :hasArrow="false"
+            @wxClick="handleClick">
+                <wx-switch 
+                    slot="right"
+                    class="switch" 
+                    v-model="value2" 
+                    @wxChange="handleChange" 
+                    :disabled="false">
+                </wx-switch>
+        </wx-cell>
+
+        <div style="padding-top: 30px">
+            <text class="wx-text">有公积金：{{ value1 }}</text>
+            <text class="wx-text">有无住房：{{ value2 }}</text>
+        </div>
     </div>
 </template>
 
 <style type="text/css">
     .wx-demo {
-        flex-direction: column;
         align-items: center;
     }
-    .switch {
-        margin-top: 100px;
+    .wx-text {
+        font-size: 32px;
     }
 </style>
 
 <script>
-    import WxSwitch from '../../packages/wx-switch/index'
+    import { WxSwitch, WxCell } from '../../index';
 
     export default {
         data () {
             return {
-                value: true,
+                value1: false,
+                value2: true,
             }
         },
 
@@ -34,9 +64,14 @@
             handleChange (checked) {
 
             },
+            handleClick (checked) {
 
+            },
         },
 
-        components: {WxSwitch}
+        components: {
+            WxSwitch,
+            WxCell
+        }
     }
 </script>
