e92c71f1fc9744052f370d3b85ab50d7d2f99f07
yangquan <yangquan@houbank.cn>
2018-07-10 11:14:46 +0800

    update demo data

--- example/picker/data.js
+++ example/picker/data.js
@@ -1,19 +1,10 @@
+const ls = [];
+for (let i =0; i< 100; i ++) {
+    ls.push({ name: '普罗米修斯' + i, value: i });
+}
 export const PICKER_DATA = {
-    list: [
-        { name: '2012', value: 0 },
-        { name: '后天', value: 1 },
-        { name: '星际穿越', value: 2 },
-        { name: '星际迷航', value: 3 },
-        { name: 'X战警', value: 4 },
-        { name: '盗梦空间', value: 5 },
-        { name: '源代码', value: 6 },
-        { name: '黑客帝国', value: 7 },
-        { name: '超验骇客', value: 8 },
-        { name: '火星救援', value: 9 },
-        { name: '阿凡达', value: 10 },
-        { name: '普罗米修斯', value: 11 },
-    ],
-    defaultValue: { name: '盗梦空间', value: 6 },
+    list: ls,
+    defaultValue: { name: '普罗米修斯5', value: 5 },
     displayValue (item) {
         return item.name;
     }
